4a7c50d36a2dd8d552bad07f9c6958c7ea57d93b
caniceiro <caniceiro@dei.uc.pt>
2024-05-26 15:21:02 +0100

    inicio pagina projeto

--- fpfrontend/src/pages/HomePage/HomePage.jsx
+++ fpfrontend/src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from './HomePage.module.css';
+import ProjectTable from '../../components/HomePageComponents/ProjectTable/ProjectTable.jsx';
+import ProtectedComponents from '../../components/auth regist/ProtectedComponents';
+import CreateProjectModal from '../../components/modals/CreateProjectModal.jsx';
 
 const HomePage = () => {
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const handleClick = () => {
+    setIsModalOpen(true);
+  }
+
   return (
     <div className={styles.homePage} >
-      HomePage
+      <ProtectedComponents>
+        <CreateProjectModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+        <button onClick={handleClick}>Click me</button>
+      </ProtectedComponents>
+      <ProjectTable/>
     </div>
   );
 };
