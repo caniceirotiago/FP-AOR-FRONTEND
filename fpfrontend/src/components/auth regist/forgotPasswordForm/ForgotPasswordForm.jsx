28366b069d62f59ac9c1216e1ae91e82ec0ac596
Vasco Castro <vascocastro@student.dei.uc.pt>
2024-06-22 19:59:40 +0100

    1

--- fpfrontend/src/components/auth regist/forgotPasswordForm/ForgotPasswordForm.jsx
+++ fpfrontend/src/components/auth regist/forgotPasswordForm/ForgotPasswordForm.jsx
@@ -1,64 +1,103 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import userService from '../../../services/userService.jsx';
-import styles from './ForgotPasswordForm.module.css';
-import {FormattedMessage} from "react-intl";
-import { FaKey } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
-import useDialogModalStore from '../../../stores/useDialogModalStore';
-import Button from '../../buttons/landingPageBtn/Button.jsx';
-
-
+import React, { useState } from "react";
+import userService from "../../../services/userService.jsx";
+import styles from "./ForgotPasswordForm.module.css";
+import { FormattedMessage, useIntl } from "react-intl";
+import { FaKey } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import useDialogModalStore from "../../../stores/useDialogModalStore";
+import Button from "../../buttons/landingPageBtn/Button.jsx";
 
 const ForgotPasswordForm = () => {
-    const navigate = useNavigate();
-    const {setIsDialogOpen, setDialogMessage, setAlertType, setOnConfirm} = useDialogModalStore();
-    const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+  const { setIsDialogOpen, setDialogMessage, setAlertType, setOnConfirm } =
+    useDialogModalStore();
+  const [email, setEmail] = useState("");
+  const intl = useIntl();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            const response = await userService.requestPasswordReset(email);
-            if (response.status === 204) {
-                setDialogMessage('Recover password Email Sent');
-                setIsDialogOpen(true);
-                setAlertType(true);
-                setOnConfirm(async () => {
-                    navigate('/');
-                });
-                return;
-            }
-            const responseBody = await response.json();
-            if(responseBody.errorMessage === "You already requested a password reset, please check your email, wait" +
-            " 30 minutes and try again, or contact the administrator"){
-                setDialogMessage('You already requested a password reset, please check your email, wait30 minutes and try again, or contact the administrator');
-            }
-            else setDialogMessage('Not able to send email, contact support.');
-            setIsDialogOpen(true);
-            setAlertType(true);
-            setOnConfirm(async () => {
-            });
-        } catch (error) {
-            console.error('Error :', error);
-        }
-    };
+  const messages = {
+    emailSent: {
+      id: "emailSent",
+      defaultMessage: "Recover password email sent",
+    },
+    emailRequestError: {
+      id: "emailRequestError",
+      defaultMessage:
+        "You already requested a password reset, please check your email, wait 30 minutes and try again, or contact the administrator",
+    },
+    emailSendFailure: {
+      id: "emailSendFailure",
+      defaultMessage: "Not able to send email, contact support.",
+    },
+  };
 
-    return (
-        <div className={styles.mainContent}>
-            <form className={styles.form}>
-                <div className={styles.banner}>
-                    <FaKey className={styles.loginIcon}/>
-                    <p className={styles.memberLoginBanner}><FormattedMessage id="recoverPassword">Recover Password</FormattedMessage></p>
-                </div>
-                <label htmlFor="email">Email</label>
-                <input className={styles.input} type="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                <div className={styles.btnDiv}>
-                    <Button onClick={handleSubmit} tradId="askForNewPassword" defaultText="Ask for new password" btnColor={"var(--btn-bolor2)"}/>
-                   <Button path="/" tradId="backToLogin" defaultText="Back to login" btnColor={"var(--btn-bolor2)"}/>
-                </div>
-            </form>
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await userService.requestPasswordReset(email);
+      if (response.status === 204) {
+        setDialogMessage(intl.formatMessage(messages.emailSent));
+        setIsDialogOpen(true);
+        setAlertType(true);
+        setOnConfirm(async () => {
+          navigate("/");
+        });
+        return;
+      }
+      const responseBody = await response.json();
+      if (
+        responseBody.errorMessage ===
+        "You already requested a password reset, please check your email, wait" +
+          " 30 minutes and try again, or contact the administrator"
+      ) {
+        setDialogMessage(intl.formatMessage(messages.emailRequestError));
+      } else {
+        setDialogMessage(intl.formatMessage(messages.emailSendFailure));
+        setIsDialogOpen(true);
+        setAlertType(true);
+        setOnConfirm(async () => {});
+      }
+    } catch (error) {
+      console.error("Error :", error);
+    }
+  };
+
+  return (
+    <div className={styles.mainContent}>
+      <form className={styles.form}>
+        <div className={styles.banner}>
+          <FaKey className={styles.loginIcon} />
+          <p className={styles.memberLoginBanner}>
+            <FormattedMessage id="recoverPassword">
+              Recover Password
+            </FormattedMessage>
+          </p>
         </div>
-    );
+        <label htmlFor="email">Email</label>
+        <input
+          className={styles.input}
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <div className={styles.btnDiv}>
+          <Button
+            onClick={handleSubmit}
+            tradId="askForNewPassword"
+            defaultText="Ask for new password"
+            btnColor={"var(--btn-bolor2)"}
+          />
+          <Button
+            path="/"
+            tradId="backToLogin"
+            defaultText="Back to login"
+            btnColor={"var(--btn-bolor2)"}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ForgotPasswordForm;
--- fpfrontend/src/components/auth regist/resetPasswordForm/ResetPasswordForm.jsx
+++ fpfrontend/src/components/auth regist/resetPasswordForm/ResetPasswordForm.jsx
@@ -1,118 +1,197 @@
-import React, { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import userService from '../../../services/userService'; 
-import { FormattedMessage} from 'react-intl';
-import useDialogModalStore from '../../../stores/useDialogModalStore';
-import styles from './ResetPasswordForm.module.css';
-import { validatePassword } from '../../../utils/validators/userValidators';
-import Button from '../../buttons/landingPageBtn/Button.jsx';
-import  DialogMultipleMessagesModalStore  from '../../../stores/useDialogMultipleMessagesModalStore';
-
+import React, { useState } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import userService from "../../../services/userService";
+import { FormattedMessage, useIntl } from "react-intl";
+import useDialogModalStore from "../../../stores/useDialogModalStore";
+import styles from "./ResetPasswordForm.module.css";
+import { validatePassword } from "../../../utils/validators/userValidators";
+import Button from "../../buttons/landingPageBtn/Button.jsx";
+import DialogMultipleMessagesModalStore from "../../../stores/useDialogMultipleMessagesModalStore";
 
 const ResetPasswordForm = () => {
-    const [passwords, setPasswords] = useState({
-        password: '',
-        confirmPassword: '',
-    });
-    const navigate = useNavigate();
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    const token = searchParams.get('token'); 
-    const { setDialogMultipleMessages, setDialogMultipleMessagesTitle, setIsDialogMultipleMessagesOpen } = DialogMultipleMessagesModalStore();
-    const { setDialogMessage, setIsDialogOpen, setAlertType, setOnConfirm } = useDialogModalStore();
-    const [passwordStrength, setPasswordStrength] = useState(0);
-    const [passwordError, setPasswordError] = useState('');
-    const [passwordStrongEnough, setpasswordStrongEnough] = useState(true);
+  const [passwords, setPasswords] = useState({
+    password: "",
+    confirmPassword: "",
+  });
+  const navigate = useNavigate();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const token = searchParams.get("token");
+  const {
+    setDialogMultipleMessages,
+    setDialogMultipleMessagesTitle,
+    setIsDialogMultipleMessagesOpen,
+  } = DialogMultipleMessagesModalStore();
+  const { setDialogMessage, setIsDialogOpen, setAlertType, setOnConfirm } =
+    useDialogModalStore();
+  const [passwordStrength, setPasswordStrength] = useState(0);
+  const [passwordError, setPasswordError] = useState("");
+  const [passwordStrongEnough, setpasswordStrongEnough] = useState(true);
+  const intl = useIntl();
+  
+  const messages = {
+    passwordRequired: {
+      id: "passwordRequired",
+      defaultMessage: "Password is required",
+    },
+    passwordsDoNotMatch: {
+      id: "passwordsDoNotMatch",
+      defaultMessage: "Passwords do not match",
+    },
+    passwordError: {
+        id: "passwordValidationError",
+        defaultMessage: "Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.",
+      },
+    passwordNotStrongEnough: {
+      id: "passwordNotStrongEnough",
+      defaultMessage: "Password is not strong enough",
+    },
+    passwordChangedSuccess: {
+      id: "passwordChangedSuccess",
+      defaultMessage: "Password Changed Successfully",
+    },
+    passwordChangeFailed: {
+      id: "passwordUpdateFail",
+      defaultMessage: "Not able to change password, contact support.",
+    },
+    validationErrors: {
+      id: "validationErrors",
+      defaultMessage: "Validation Errors",
+    },
+  };
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setPasswords({ ...passwords, [name]: value });
-        if (name === 'password') {
-            checkPasswordStrength(value);
-            if (!validatePassword(value)) {
-                setPasswordError('Password must be at least 8 characters long, include an uppercase letter, a lowercase letter, a number, and a special character.');
-              } else {
-                setPasswordError('');
-              }
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPasswords({ ...passwords, [name]: value });
+    if (name === "password") {
+      checkPasswordStrength(value);
+      if (!validatePassword(value)) {
+        setPasswordError(
+            intl.formatMessage(messages.passwordError)
+          );
+      } else {
+        setPasswordError("");
+      }
+    }
+  };
+
+  const checkPasswordStrength = (password) => {
+    const { isValid, errors, score } = validatePassword(password);
+    setPasswordStrength(score);
+    setPasswordError(errors.join(" "));
+    setpasswordStrongEnough(isValid);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newErrors = {
+      1: !passwords.password
+        ? intl.formatMessage(messages.passwordRequired)
+        : "",
+      2:
+        passwords.password !== passwords.confirmPassword
+          ? intl.formatMessage(messages.passwordsDoNotMatch)
+          : "",
+      3:
+        passwordError !== ""
+          ? intl.formatMessage(messages.passwordError, { passwordError })
+          : "",
+      4: passwordStrongEnough
+        ? ""
+        : intl.formatMessage(messages.passwordNotStrongEnough),
+    };
+
+    const isValid = Object.keys(newErrors).every((key) => !newErrors[key]);
+    if (isValid) {
+      try {
+        const response = await userService.resetPassword(
+          token,
+          passwords.password
+        );
+        if (response.status === 204) {
+            setDialogMessage(intl.formatMessage(messages.passwordChangedSuccess));
+          setIsDialogOpen(true);
+          setAlertType(true);
+          setOnConfirm(async () => {
+            navigate("/");
+          });
+          return;
         }
-    };
+        setDialogMessage(intl.formatMessage(messages.passwordChangeFailed));
+        setIsDialogOpen(true);
+        setAlertType(true);
+        setOnConfirm(async () => {
+          navigate("/");
+        });
+      } catch (error) {
+        console.error("Error :", error);
+      }
+    } else {
+      const errorMessages = Object.entries(newErrors)
+        .filter(([key, value]) => value !== "")
+        .map(([key, value]) => `${key}: ${value}`);
+      setDialogMultipleMessages(errorMessages);
+      setDialogMultipleMessagesTitle(intl.formatMessage(messages.validationErrors));
+      setIsDialogMultipleMessagesOpen(true);
+    }
+  };
 
-    const checkPasswordStrength = (password) => {
-        const { isValid, errors, score } = validatePassword(password);
-        setPasswordStrength(score);
-        setPasswordError(errors.join(' '));
-        setpasswordStrongEnough(isValid);
-    };
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const newErrors = {
-            Password: !passwords.password ? ' is required' : '',
-            Confirmation: passwords.password !== passwords.confirmPassword ? ' passwords do not match' : '',
-            Passwords: passwordError !=='' ? passwordError : '',
-            Weak: passwordStrongEnough ? '' : 'Password is not strong enough'
-       };
-       const isValid = Object.keys(newErrors).every((key) => !newErrors[key]);
-       if (isValid) {
-        try {
-            const response = await userService.resetPassword(token, passwords.password);
-            if (response.status === 204) {
-                setDialogMessage('Password Changed Successfully');
-                setIsDialogOpen(true);
-                setAlertType(true);
-                setOnConfirm(async () => {
-                    navigate('/');
-                });
-                return;
-            }
-            const responseBody = await response.json();
-            
-            setDialogMessage('Not able to change password, contact support.');
-            setIsDialogOpen(true);
-            setAlertType(true);
-            setOnConfirm(async () => {
-                navigate('/');
-            });
-        } catch (error) {
-            console.error('Error :', error);
-        }
-       }
-       else {
-        const errorMessages = Object.entries(newErrors)
-           .filter(([key, value]) => value !== '') 
-           .map(([key, value]) => `${key}: ${value}`); 
-        setDialogMultipleMessages(errorMessages);
-        setDialogMultipleMessagesTitle('Validation Errors');
-        setIsDialogMultipleMessagesOpen(true);
-     }
-
-        
-    };
-
-    return (
-        <div className={styles.mainContent}>
-            <form className={styles.form} >
-                <h2><FormattedMessage id="changePassword">Change Password</FormattedMessage></h2>
-                <label>
-                    <FormattedMessage id="newPassword"> New Password:</FormattedMessage>
-                    <input name="password" className={styles.input} type="password" value={passwords.password} onChange={handleChange} required minLength={4}/>
-                </label>
-                <div className={styles.passwordStrength}>
-                            <div style={{ width: `${passwordStrength * 25}%`, backgroundColor: passwordStrength >= 3 ? 'green' : 'yellow', height: '5px', marginTop: '5px' }}></div>
-                        </div>
-                        {passwordError && (
-                            <div className={styles.passwordError}>
-                                {passwordError}
-                            </div>
-                        )}
-                <label>
-                <FormattedMessage id="confirmPassword">Confirm Password:</FormattedMessage>
-                    <input name="confirmPassword" className={styles.input} type="password" value={passwords.confirmPassword} onChange={handleChange} required minLength={4}/>
-                </label>
-                <Button onClick={handleSubmit} tradId="changePassword" defaultText="Change Password" btnColor={"var(--btn-bolor2)"}/>
-            </form>
+  return (
+    <div className={styles.mainContent}>
+      <form className={styles.form}>
+        <h2>
+          <FormattedMessage id="changePassword">
+            Change Password
+          </FormattedMessage>
+        </h2>
+        <label>
+          <FormattedMessage id="newPassword"> New Password:</FormattedMessage>
+          <input
+            name="password"
+            className={styles.input}
+            type="password"
+            value={passwords.password}
+            onChange={handleChange}
+            required
+            minLength={4}
+          />
+        </label>
+        <div className={styles.passwordStrength}>
+          <div
+            style={{
+              width: `${passwordStrength * 25}%`,
+              backgroundColor: passwordStrength >= 3 ? "green" : "yellow",
+              height: "5px",
+              marginTop: "5px",
+            }}
+          ></div>
         </div>
-    );
+        {passwordError && (
+          <div className={styles.passwordError}>{passwordError}</div>
+        )}
+        <label>
+          <FormattedMessage id="confirmPassword">
+            Confirm Password:
+          </FormattedMessage>
+          <input
+            name="confirmPassword"
+            className={styles.input}
+            type="password"
+            value={passwords.confirmPassword}
+            onChange={handleChange}
+            required
+            minLength={4}
+          />
+        </label>
+        <Button
+          onClick={handleSubmit}
+          tradId="changePassword"
+          defaultText="Change Password"
+          btnColor={"var(--btn-bolor2)"}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default ResetPasswordForm;
