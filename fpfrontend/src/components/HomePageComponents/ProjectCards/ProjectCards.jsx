f84a023b24a269b93ac0981f9029e5e6b1845a7c
caniceiro <caniceiro@dei.uc.pt>
2024-07-05 17:57:10 +0100

    alterações Pagina Proj

--- fpfrontend/src/components/HomePageComponents/ProjectCards/ProjectCards.jsx
+++ fpfrontend/src/components/HomePageComponents/ProjectCards/ProjectCards.jsx
@@ -6,26 +6,8 @@ import useConfigurationStore from "../../../stores/useConfigurationStore";
 import useLoginModalStore from "../../../stores/useLoginModalStore";
 import useProjectStore from "../../../stores/useProjectStore";
 import { FaChartGantt } from "react-icons/fa6";
+import {stateColors, stateColorsBriefcaseBackground} from "../../../utils/colors/projectColors";
 
-const stateColors = {
-  PLANNING: "var(--color-planning)",
-  READY: "var(--color-ready)",
-  IN_PROGRESS: "var(--color-in-progress)",
-  FINISHED: "var(--color-finished)",
-  CANCELLED: "var(--color-cancelled)",
-};
-const stateColorsBriefcaseBackground = {
-  PLANNING:
-    "linear-gradient(45deg, var(--color-planning) 0%, var(--briefcase-base-color)  100%)",
-  READY:
-    "linear-gradient(45deg, var(--color-ready) 0%, var(--briefcase-base-color) 100%",
-  IN_PROGRESS:
-    "linear-gradient(45deg, var(--color-in-progress) 0%, var(--briefcase-base-color) 100%)",
-  FINISHED:
-    "linear-gradient(45deg, var(--color-finished) 0%, var(--briefcase-base-color) 100%)",
-  CANCELLED:
-    "linear-gradient(45deg, var(--color-cancelled) 0%, var(--briefcase-base-color) 100%)",
-};
 
 function ProjectCards({ projects, isAuthenticated }) {
   const navigate = useNavigate();
---
+++ fpfrontend/src/utils/colors/projectColors.jsx
@@ -0,0 +1,19 @@
+export const stateColors = {
+    PLANNING: "var(--color-planning)",
+    READY: "var(--color-ready)",
+    IN_PROGRESS: "var(--color-in-progress)",
+    FINISHED: "var(--color-finished)",
+    CANCELLED: "var(--color-cancelled)",
+  };
+  export const stateColorsBriefcaseBackground = {
+    PLANNING:
+      "linear-gradient(45deg, var(--color-planning) 0%, var(--briefcase-base-color)  100%)",
+    READY:
+      "linear-gradient(45deg, var(--color-ready) 0%, var(--briefcase-base-color) 100%",
+    IN_PROGRESS:
+      "linear-gradient(45deg, var(--color-in-progress) 0%, var(--briefcase-base-color) 100%)",
+    FINISHED:
+      "linear-gradient(45deg, var(--color-finished) 0%, var(--briefcase-base-color) 100%)",
+    CANCELLED:
+      "linear-gradient(45deg, var(--color-cancelled) 0%, var(--briefcase-base-color) 100%)",
+  };
