bb5224f32b49c84cebafe4b5c842e289f3bb39d7
caniceiro <caniceiro@dei.uc.pt>
2024-07-08 12:44:23 +0100

    porta

--- fpfrontend/src/stores/useDomainStore.jsx
+++ fpfrontend/src/stores/useDomainStore.jsx
@@ -1,8 +1,8 @@
 import {create} from 'zustand';
 
 const useDomainStore = create((set) => ({
-    httpsDomain: "https://localhost:8080/FPBackend",
-    wwsDomain: "wss://localhost:8080/FPBackend"
+    httpsDomain: "https://localhost:8443/FPBackend",
+    wwsDomain: "wss://localhost:8443/FPBackend"
 }));
 
 export default useDomainStore;
