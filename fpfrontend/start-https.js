0f80c21b5eded060c450cadf4a9a216e74f14326
caniceiro <caniceiro@dei.uc.pt>
2024-07-09 10:30:34 +0100

    dg

--- fpfrontend/start-https.js
+++ fpfrontend/start-https.js
@@ -1,5 +1,6 @@
 process.env.HTTPS = "true";
-process.env.SSL_CRT_FILE = "C:\\Users\\Familia\\Documents\\GIT Kraken\\AORFP_cert.pem";
-process.env.SSL_KEY_FILE = "C:\\Users\\Familia\\Documents\\GIT Kraken\\AORFP_private_key.pem";
-
-require("react-scripts/scripts/start");
+process.env.SSL_CRT_FILE =
+  "/Users/wildfly-30.0.1.Final/standalone/configuration/AORFP_cert.crt";
+process.env.SSL_KEY_FILE =
+  "/Users/wildfly-30.0.1.Final/standalone/configuration/AORFP_private_key.pem";
+  require("react-scripts/scripts/start");
